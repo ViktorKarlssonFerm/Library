4f38fae1f76c25f4d3b418939e32c3a97770e1d7
Viktor Karlsson Ferm <viktor.karlssonferm@elev.ga.ntig.se>
2019-09-26 22:18:40 +0200

    Added a Java database connection file

---
+++ src/JavaDB.java
@@ -0,0 +1,96 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
+public class JavaDB {
+	
+	private Connection con;
+	Object[]fields;
+	
+	public JavaDB(String host, String user, String password, String database){
+		
+		try {
+			
+			Class.forName("org.gjt.mm.mysql.Driver").newInstance();
+			con = DriverManager.getConnection("jdbc:mysql://" + host + "/" + database, user, password);
+			
+		}catch(Exception error) {
+			
+			JOptionPane.showMessageDialog(null, error);
+			
+		}
+		
+	}
+	
+	public Object[][] getData(String SQL){
+		
+		Object[][]data = null;
+		try {
+			
+			Statement statement = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			ResultSet rs = statement.executeQuery(SQL);
+			ResultSetMetaData md = rs.getMetaData();
+			fields = new Object[md.getColumnCount()];
+			for(int i = 0; i < fields.length; i++) {
+				
+				fields[i] = md.getColumnName(i + 1);
+				System.out.println(fields[i]);
+				
+			}
+			int amountOfRows = 0;
+			while(rs.next()) {
+				
+				amountOfRows++;
+				
+			}
+			data = new Object[amountOfRows][md.getColumnCount()];
+			rs.beforeFirst();
+			int row = 0;
+			while(rs.next()) {
+				
+				for(int j = 0; j < md.getColumnCount(); j++) {
+					
+					data[row][j] = rs.getString(j + 1);
+					
+				}
+				
+				row++;
+				
+			}
+			
+		}catch(Exception error) {
+			
+			JOptionPane.showMessageDialog(null, error);
+			
+		}
+		
+		return data;
+		
+	}
+	
+	public Object[] getFields() {
+		
+		return fields;
+		
+	}
+	
+	public void execute(String SQL) {
+		
+		try {
+			
+			Statement statement = con.createStatement();
+			statement.execute(SQL);
+			
+		}catch(Exception error) {
+			
+			JOptionPane.showMessageDialog(null, error);
+			
+		}
+		
+	}
+	
+}
---
+++ src/borrowGUI.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class borrowGUI extends JFrame {
+	
+	
+	
+	public borrowGUI(){
+		
+		
+		
+	}
+
+}
