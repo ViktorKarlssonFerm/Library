da2df1b5f6534481a79aceb808a9ad40f951aa52
Viktor Karlsson Ferm <viktor.karlssonferm@elev.ga.ntig.se>
2019-09-26 15:29:46 +0200

    Added text fields and a button to the login GUI

--- src/gui.java
+++ src/gui.java
@@ -1,9 +1,13 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
 public class gui extends JFrame{
 	
@@ -11,10 +15,32 @@ public gui() {
 		
 		setPreferredSize(new Dimension(1000, 600));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		JPanel pane = new JPanel();
-		pane.setLayout(new BorderLayout(0, 0));
-		pane.setBackground(new Color(255,0,0));
+		
+		JPanel form = new JPanel();
+		form.setBackground(new Color(0,255,0));
+		form.setPreferredSize(new Dimension(250, 250));
+		
+		JPanel loginform = new JPanel();
+		loginform.setPreferredSize(new Dimension(250, 250));
+		loginform.setBorder(new LineBorder(new Color(234, 234, 43), 5));
+		
+		JTextField fnamn = new JTextField();
+		fnamn.setPreferredSize(new Dimension(150,30));
+		fnamn.setFont(new Font("",Font.PLAIN,18));
+		
+		JTextField lnamn = new JTextField();
+		lnamn.setPreferredSize(new Dimension(150,30));
+		lnamn.setFont(new Font("",Font.PLAIN,18));
+		
+		JButton login = new JButton("Log in");
+		login.setPreferredSize(new Dimension(100,30));
+		
 		setTitle("Library");
+		add(form, BorderLayout.WEST);
+		form.add(loginform);
+		loginform.add(fnamn);
+		loginform.add(lnamn);
+		loginform.add(login);
 		pack();
 		setVisible(true);
 		
